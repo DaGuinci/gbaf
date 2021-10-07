ed2c982e9bc1dbdbac92fa4b8d4517f890b2c60e
DaGuinci <daguinci@mailo.com>
2021-10-07 13:13:11 +0200

    favicon

--- view/template.php
+++ view/template.php
@@ -6,6 +6,7 @@
 		<meta charset="utf-8"/>
 		<title><?= $title ?></title>
 		<link href="./public/CSS/styles.css" rel="stylesheet" />
+		<link rel="icon" type="image/png" href="./public/images/logoGbaf.png" />
 	</head>
 	
 	<body>
